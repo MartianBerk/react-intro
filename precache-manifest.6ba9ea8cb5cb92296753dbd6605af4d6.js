5a7a4871a6c3ac44a8eb47a137496978142ad66a
martianberk <baker.bsc@gmail.com>
2019-08-17 22:18:09 +0100

    Updates

---
+++ precache-manifest.6ba9ea8cb5cb92296753dbd6605af4d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1d9eb024cb1c25b6f57e0766ae4c2fb",
+    "url": "/react-intro/index.html"
+  },
+  {
+    "revision": "bb5642521e85a2fe7556",
+    "url": "/react-intro/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c03084515bffa4c58897",
+    "url": "/react-intro/static/js/2.c89104ed.chunk.js"
+  },
+  {
+    "revision": "bb5642521e85a2fe7556",
+    "url": "/react-intro/static/js/main.e6d7de2a.chunk.js"
+  },
+  {
+    "revision": "65c086c471ec1169d1a7",
+    "url": "/react-intro/static/js/runtime~main.42078186.js"
+  }
+]);
